632868250471d21959143a24deb7a9546e4200bd
mdak505 <mdakhussain505@gmail.com>
2025-04-09 04:41:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function handleSubmit(event) {
   event.preventDefault();
   alert("Your message has been sent. We will get back to you soon!");
 }
+// Animated Counter Script
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / 100;
+
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 30);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  // Start the counter animation when scrolled into view
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      updateCounter();
+      observer.disconnect(); // Run once
+    }
+  });
+
+  observer.observe(counter);
+});
