3d8a3d92a630658923fe94e40beddf9a45837c22
mdak505 <mdakhussain505@gmail.com>
2025-04-09 04:10:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  alert("GrowHealth RCM helps you get paid faster with less hassle!");
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Thanks for contacting us! We'll get back to you soon.");
+}
